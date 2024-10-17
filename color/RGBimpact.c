1ed93830593319e34b54d5c7d89d3c6958612630
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2024-10-17 22:39:28 +0800

    Update RGBimpact.c

--- color/RGBimpact.c
+++ color/RGBimpact.c
@@ -1,6 +1,5 @@
-#include "chemistry.h"
 #include "color.h"
-struct impact(){
+struct impact({
  char impact0 = "element1 + element2 + element3 + element4";
  char impact1 = "element5 - element6 - element7 - element8";
  char impact2 = "element9 * element10 * element11 *element12";
@@ -10,6 +9,6 @@ struct impact(){
 struct impact9(){
  char impact5 = "impact0 * impact1" + "impact2 * impact3";
  char impact6 = "impact1 - impact2" + "impact3 - impact4";
- char impact7 = "impact2 / impact3" + "impact4 / impact0";
- char impact8 = "impact3 % impact4" + "impact1 % impact2";
+ char impact7 = "impact0 / impact1" + "impact3 / impact4";
+ char impact8 = "impact1 % impact2" + "impact3 % impact4";
 }
