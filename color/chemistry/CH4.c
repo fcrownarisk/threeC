54159eae8e9571b776587737852dea8f034331cf
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2025-06-23 20:30:11 +0800

    Update CH4.c

--- color/chemistry/CH4.c
+++ color/chemistry/CH4.c
@@ -4,7 +4,7 @@ void CH4(double C, double H) {
 }
 void C5H12(double C, double H) {
    const C5 = C * C * 2 + C;
-   const H4 = H*H + H*H + H*H + H*H;
+   const H12 = ( H * H + H * H ) * 2;
 }
 void C17H36(double C, double H) {
     const C17 = (C * C) * 4 + C;
