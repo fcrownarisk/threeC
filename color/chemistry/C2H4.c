446540188ef5a71ddc629782c93bcbb90db676b7
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2025-06-23 20:24:52 +0800

    Update C2H4.c

--- color/chemistry/C2H4.c
+++ color/chemistry/C2H4.c
@@ -1,12 +1,12 @@
-void C2H4(float C, double H) {
-    double C2 = C*C;
-    double H4 = H*H + H*H;
+void C2H4(float C, float H) {
+    const C2 = C*C;
+    const H4 = H*H + H*H;
 }
-void C6H8(float C, double H) {
-    double C2 = C*C;
-    double H6 = H*H + H*H + H*H;
+void C6H8(float C, float H) {
+    const C2 = C*C;
+    const H6 = H*H + H*H + H*H;
 }
-void C14H16(float C, double H) {
-    double C14 = ( C * C ) * 7
-    double H16 = ( H * H ) * 4
+void C14H16(float C, float H) {
+    const C14 = ( C * C ) * 7;
+    const H16 = ( H * H ) * 4;
 } 
