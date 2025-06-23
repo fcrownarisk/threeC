2556f7dcf670f8bdbd68f7603531b72aedbf7a4f
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2025-06-23 20:20:29 +0800

    Update C2H4.c

--- color/chemistry/C2H4.c
+++ color/chemistry/C2H4.c
@@ -1,4 +1,12 @@
-void C2H6(float C, double H) {
+void C2H4(float C, double H) {
     double C2 = C*C;
     double H4 = H*H + H*H;
 }
+void C6H8(float C, double H) {
+    double C2 = C*C;
+    double H6 = H*H + H*H + H*H;
+}
+void C14H16(float C, double H) {
+    double C14 = ( C * C ) * 7
+    double H16 = ( H * H ) * 4
+} 
