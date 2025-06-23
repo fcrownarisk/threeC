1a9b03e0e4c276190216c268d9a20f194915b71f
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2025-06-23 20:26:20 +0800

    Update CH4.c

--- color/chemistry/CH4.c
+++ color/chemistry/CH4.c
@@ -1,12 +1,12 @@
-void CH4(int C, double H) {
-    int C1 = C;
-    double H4 = H*H + H*H;
+void CH4(double C, double H) {
+    double C1 = C;
+    double H4 = H * H + H * H;
 }
-void C5H12(int C, double H) {
-   int C5 = C + C * C;
+void C5H12(double C, double H) {
+   double C5 = C * C * 2 + C;
    double H4 = H*H + H*H + H*H + H*H;
 }
-void C17H36(int C, double H) {
-    int C17 = 17;
-    double H36 = 36;
+void C17H36(double C, double H) {
+    double C17 = (C * C) * 4 + C;
+    double H36 = ( H * H ) * 6;
 }
